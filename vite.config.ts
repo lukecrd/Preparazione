c5e899a0d69edf9b4dc0fd803c264d51752ff07c
lukecrd <54847090+lukecrd@users.noreply.github.com>
2026-08-25 11:42:48 +0200

    Fix Vite config for base path and resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base:'/Preparazione/',
-    resolve: {
+      resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
